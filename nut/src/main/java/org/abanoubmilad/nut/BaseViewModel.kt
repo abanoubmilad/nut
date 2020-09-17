2dbc043b5403bd99c67bb6c8cc4525235e1a469b
abanoubmilad <abanoubcs@gmail.com>
2020-09-18 01:14:37 +0200

    Remove extra VM funs

--- nut/src/main/java/org/abanoubmilad/nut/BaseViewModel.kt
+++ nut/src/main/java/org/abanoubmilad/nut/BaseViewModel.kt
@@ -1,12 +1,8 @@
 package org.abanoubmilad.nut
 
-import android.view.View
 import androidx.annotation.CallSuper
-import androidx.annotation.StringRes
 import androidx.databinding.Observable
 import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 
@@ -23,38 +19,6 @@ open class BaseViewModel : ViewModel(), ISync, IObserve {
 
     override val observablesMap by lazy {
         hashMapOf<ObservableField<*>, Observable.OnPropertyChangedCallback>()
-    }
-
-    private val _message =
-        SingleLiveEvent<@StringRes Int>()
-    val message: LiveData<Int> = _message
-
-    private val _loading =
-        MutableLiveData<Boolean>(false)
-    val loading: LiveData<Boolean> = _loading
-
-    fun showLoading(show: Boolean) {
-        _loading.postValue(show)
-    }
-
-    fun showLoading() {
-        _loading.postValue(true)
-    }
-
-    fun hideLoading() {
-        _loading.postValue(false)
-    }
-
-    fun fireMessage(@StringRes msg: Int) {
-        _message.postValue(msg)
-    }
-
-    fun visibleOrInvisible(visible: Boolean): Int {
-        return if (visible) View.VISIBLE else View.INVISIBLE
-    }
-
-    fun visibleOrGone(visible: Boolean): Int {
-        return if (visible) View.VISIBLE else View.GONE
     }
 
     @CallSuper
