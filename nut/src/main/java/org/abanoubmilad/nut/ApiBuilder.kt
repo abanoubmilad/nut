b5f0bc100a4d331706e34bf1e2d465205297750a
abanoubcs@gmail.com <abanoubcs@gmail.com>
2021-02-08 20:24:29 +0200

    v0.53 update gradle

--- nut/src/main/java/org/abanoubmilad/nut/ApiBuilder.kt
+++ nut/src/main/java/org/abanoubmilad/nut/ApiBuilder.kt
@@ -5,7 +5,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -75,7 +75,7 @@ open class ApiBuilder(
                     addConverterFactory(GsonConverterFactory.create())
 
             }
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
             .client(client)
             .build()
     }
